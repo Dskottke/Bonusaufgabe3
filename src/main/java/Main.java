1916f373da51a8f03da609bb15fab7b28ab0b73d
davidskottke <davidskottke@googlemail.com>
2022-10-03 14:17:51 +0200

    initialize commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    //Schreibe eine Methode die einen Eingabestring rückwärts ausgibt.
+    //Gibt den folgenden Array aufsteigend sortiert aus: {6, 2, 8, 3, 9, 6}
+    //Schreibe eine Methode die einen String nach , separiert und als Array zurück gibt.
+    //Schreibe eine Methode die Quersumme einer Ganzzahl berechnet.
+    //Gib eine Ganzzahl (1-100) als römische Zahl aus (12 = XII)
+
+}
